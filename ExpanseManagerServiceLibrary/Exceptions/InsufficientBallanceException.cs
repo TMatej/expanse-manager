848fc2c9b91567bba30b2029e256290f642c2f9c
Matej Turek <469107@fi.muni.cz>
2021-07-22 14:40:03 +0200

    [ADD] Exceptions for service layer created

---
+++ ExpanseManagerServiceLibrary/Exceptions/InsufficientBallanceException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpanseManagerServiceLibrary.Exceptions
+{
+    public class InsufficientBallanceException : Exception
+    {
+        public InsufficientBallanceException()
+        {
+        }
+
+        public InsufficientBallanceException(string message) : base(message)
+        {
+        }
+
+        public InsufficientBallanceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InsufficientBallanceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ExpanseManagerServiceLibrary/Exceptions/UnknownExchangeRateException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpanseManagerServiceLibrary.Exceptions
+{
+    public class UnknownExchangeRateException : Exception
+    {
+        public UnknownExchangeRateException()
+        {
+        }
+
+        public UnknownExchangeRateException(string message) : base(message)
+        {
+        }
+
+        public UnknownExchangeRateException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnknownExchangeRateException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
