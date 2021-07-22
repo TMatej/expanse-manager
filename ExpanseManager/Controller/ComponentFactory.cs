41621e7f7627a48f2de5222dff992b8f90fe293f
Matej Turek <469107@fi.muni.cz>
2021-07-22 14:38:16 +0200

    [ADD] Component factory created (may be replaced by DI)

---
+++ ExpanseManager/Controller/ComponentFactory.cs
@@ -0,0 +1,64 @@
+﻿using ExpanseManager.Controller.Services.Accounts;
+using ExpanseManagerDBLibrary.Repositories.Accounts;
+using ExpanseManagerDBLibrary.Repositories.Currencies;
+using ExpanseManagerDBLibrary.Repositories.CurrencyConversions;
+using ExpanseManagerDBLibrary.Repositories.Payments;
+using ExpanseManagerServiceLibrary.Services.Accounts;
+using ExpanseManagerServiceLibrary.Services.Currencies;
+using ExpanseManagerServiceLibrary.Services.CurrencyConversions;
+using ExpanseManagerServiceLibrary.Services.Payments;
+using ExpanseManagerServiceLibrary.Services.Security;
+using ExpanseManagerServiceLibrary.Services.Transactions;
+
+namespace ExpanseManager.Controller
+{
+    public static class ComponentFactory
+    {
+        public static IPaymentRepository GetPaymentRepository()
+        {
+            return new PaymentRepositoryImpl();
+        }
+
+        public static IAccountRepository GetAccountRepository() 
+        {
+            return new AccountRepositoryImpl();
+        }
+
+        public static ICurrencyRepository GetCurrencyRepository()
+        {
+            return new CurrencyRepositoryImpl();
+        }
+        public static ICurrencyConversionRepository GetCurrencyConversionRepository()
+        {
+            return new CurrencyConversionRepositoryImpl();
+        }
+        public static IPasswordService GetPasswordService()
+        {
+            return new PasswordService();
+        }
+        public static IPaymentService GetPaymentService()
+        {
+            return new PaymentServiceImpl(GetPaymentRepository());
+        }
+        public static IAccountService GetAccountService()
+        {
+            return new AccountServiceImpl(GetAccountRepository());
+        }
+        public static ICurrencyService GetCurrencyService()
+        {
+            return new CurrencyServiceImpl(GetCurrencyRepository());
+        }
+        public static ICurrencyConversionService GetCurrencyConversionService()
+        {
+            return new CurrencyConversionServiceImpl(GetCurrencyConversionRepository());
+        }
+        public static ITransactionService GetTransactionService()
+        {
+            return new TransactionServiceImpl(GetAccountService(), GetPaymentService(), GetCurrencyConversionService());
+        }
+        public static AccountServiceView GetAccountServiceView()
+        {
+            return new AccountServiceView(GetAccountService(), GetPasswordService(), GetCurrencyService());
+        }
+    }
+}
