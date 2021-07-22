7f78c0c0f06fc485b1bb755ff5969d3b265339ce
Matej Turek <469107@fi.muni.cz>
2021-07-22 14:38:51 +0200

    [FIX] Added formatting parameter to method.

--- ExpanseManagerDBLibrary/Models/Interfaces/IJsonConvertable.cs
+++ ExpanseManagerDBLibrary/Models/Interfaces/IJsonConvertable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace ExpanseManagerDBLibrary.Models
 {
     public interface IJsonConvertable
     {
-        string ToJSON();
+        string ToJSON(Formatting formatting);
     }
 }
