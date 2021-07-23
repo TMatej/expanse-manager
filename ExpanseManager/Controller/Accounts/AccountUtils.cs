a46da4c243f0ff9bfc94c1b38c5ba7086790f867
Matej Turek <469107@fi.muni.cz>
2021-07-23 16:44:30 +0200

    [ADD/FIX] Some utl function replaced from AccountServiceview to new Classes

---
+++ ExpanseManager/Controller/Accounts/AccountUtils.cs
@@ -0,0 +1,58 @@
+﻿using ExpanseManager.ConsoleView;
+using ExpanseManagerDBLibrary.Models;
+using ExpanseManagerDBLibrary.Models.Enums;
+using System;
+
+namespace ExpanseManager.Controller.Accounts
+{
+    public class AccountUtils
+    {
+        public static string CreateName(string request = "Insert your full name.")
+        {
+            string name;
+
+            Console.WriteLine();
+            BasicOutputMessages.PrintResponseMessage(request);
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine();
+
+            return name;
+        }
+
+        public static Sex ChooseGender(string request = "Pick your gender.")
+        {
+            string input;
+
+            Console.WriteLine();
+            BasicOutputMessages.PrintResponseMessage(request);
+            Console.WriteLine("Options: ");
+            Console.WriteLine("\t1\tMale");
+            Console.WriteLine("\t2\tFemale");
+            Console.WriteLine("\t3\tOther");
+            Console.Write("Option: ");
+            input = Console.ReadLine().Trim();
+            Console.WriteLine();
+            var gender = input switch
+            {
+                "1" => Sex.Male,
+                "2" => Sex.Female,
+                _ => Sex.Other,
+            };
+
+            return gender;
+        }
+                        
+        public static void CoppyAccount(AccountModel source, AccountModel destination)
+        {
+            destination.Id = source.Id;
+            destination.UserName = source.UserName;
+            destination.PasswordHash = source.PasswordHash;
+            destination.Name = source.Name;
+            destination.Gender = source.Gender;
+            destination.Ballance = source.Ballance;
+            destination.Currency = source.Currency;
+            destination.LastTimeLogedIn = source.LastTimeLogedIn.Value;
+        }
+    }
+}
---
+++ ExpanseManager/Controller/ViewUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpanseManager.Controller
+{
+    public class ViewUtils
+    {
+        public static string GetHiddenConsoleInput()
+        {
+            StringBuilder input = new StringBuilder();
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                if (key.Key == ConsoleKey.Backspace && input.Length > 0)
+                {
+                    input.Remove(input.Length - 1, 1);
+                }
+                else if (key.Key != ConsoleKey.Backspace)
+                {
+                    input.Append(key.KeyChar);
+                }
+            }
+            return input.ToString();
+        }
+    }
+}
