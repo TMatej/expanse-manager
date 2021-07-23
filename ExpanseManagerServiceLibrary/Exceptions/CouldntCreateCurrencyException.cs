905f423efe5a5a851e8391189a3f8ece15237681
Matej Turek <469107@fi.muni.cz>
2021-07-23 16:43:54 +0200

    [ADD] Exception which is thrown when someone tries to create a duplicate currency

---
+++ ExpanseManagerServiceLibrary/Exceptions/CouldntCreateCurrencyException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Common;
+using System.Runtime.Serialization;
+
+namespace ExpanseManagerServiceLibrary.Exceptions
+{
+    [Serializable]
+    public class CouldntCreateCurrencyException : Exception
+    {
+        private DbException ex;
+
+        public CouldntCreateCurrencyException()
+        {
+        }
+
+        public CouldntCreateCurrencyException(DbException ex)
+        {
+            this.ex = ex;
+        }
+
+        public CouldntCreateCurrencyException(string message) : base(message)
+        {
+        }
+
+        public CouldntCreateCurrencyException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CouldntCreateCurrencyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
